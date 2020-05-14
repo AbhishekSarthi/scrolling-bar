84c7eaa1dbbafe188c612f93f9da109dfaa792d4
AbhishekSarthi <59399188+AbhishekSarthi@users.noreply.github.com>
2020-05-15 04:42:32 +0530

    Add files via upload

---
+++ scroll.js
@@ -0,0 +1,14 @@
+ const scrollLine = document.querySelector('.scroll');
+
+function fillScrollLine(){
+
+	const windowHeight = window.innerHeight;
+	const fullHeight   = document.body.clientHeight;
+	const scrolled     = window.scrollY;
+	const percentScrolled =(scrolled / (fullHeight -windowHeight))* 100;
+
+	scrollLine.style.width =percentScrolled +'%';
+
+}
+
+  window.addEventListener('scroll', fillScrollLine);
